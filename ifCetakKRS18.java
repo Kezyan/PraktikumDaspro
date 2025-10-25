a0d10e862d1062c628bb1ebee87bc6549669b0e8
“Kezyan” <anandito127@example.com>
2025-10-25 16:45:13 +0700

    Percobaan 1

---
+++ ifCetakKRS18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifCetakKRS18 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = scanner.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+
+        }
+    }
+}
