313da2874a8bb846263797b0a34c18f2b9ca390a
“Kezyan” <anandito127@example.com>
2025-10-25 17:06:57 +0700

    Modifikasi Percobaan 1

---
+++ ifCetakKRS17.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ifCetakKRS17 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = scanner.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+
+        }
+        else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
+    }
+}
